3f9c1f14f130b077d5df42da686d9ea37890728b
Ewout van der Linden <IonKiwi@users.noreply.github.com>
2020-11-01 12:03:01 +0100

    fix casing

--- build-container.ps1
+++ build-container.ps1
@@ -1,3 +1,3 @@
-# & docker build -f ".\DockerTokenAuthentication\Dockerfile" --force-rm -t DockerTokenAuthentication "."
-# & docker build -f ".\DockerTokenAuthentication\Dockerfile.Windows" --force-rm -t DockerTokenAuthentication "."
+# & docker build -f ".\DockerTokenAuthentication\Dockerfile" --force-rm -t dockertokenauthentication "."
+# & docker build -f ".\DockerTokenAuthentication\Dockerfile.Windows" --force-rm -t dockertokenauthentication "."
 & 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe' .\DockerTokenAuthentication\DockerTokenAuthentication.csproj /t:ContainerBuild /p:Configuration=Release
